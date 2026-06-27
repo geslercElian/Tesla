95cedf92c51143d76d72069ae2eca6adc79be621
geslercalero1@gmail.com <geslercalero1@gamil.com>
2026-06-26 19:55:39 -0600

    Comienzo de las funciones de la calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,11 +1,23 @@
 print("    ALEXANDER NEWTOWN       ")
-print(" Bienvenido a la calculadora de la familia Hellsing")
+print(" Bienvenido a la calculadora de la familia Hellsing  ")
 print("=============================================")
 print("Eliga la operacion que desea realizar (Elegir el número de la operacion): ")
 print("1. Suma")
 print("2. Resta")
 print("3. Multiplicacion")
 print("4. Division")
-op = int(input("Ingrese la operacion que desea realizar: "))
+op = str(input("Ingrese la operacion que desea realizar: "))
 n1 = float(input("Ingrese el primer numero: "))
 n2 = float(input("Ingrese el segundo numero: "))
+
+def swich():
+    if op == "1":
+        return n1 + n2
+    if op == "2":
+        return n1 - n2
+    if op == "3":
+        return n1 * n2
+    
+
+
+
