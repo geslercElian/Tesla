056738c1dec1aeb10d198889f9a4b5e2218c676e
geslercalero1@gmail.com <geslercalero1@gamil.com>
2026-06-29 17:44:29 -0600

    Input del usuario

---
+++ Contra.py
@@ -0,0 +1,12 @@
+import random as rd
+
+
+lista_de_pw = ["Nirvana", "Paradise", "The Rollings Stone", "Guns n Roses"]
+
+
+edad = 18
+
+ctUsuario = int(input("Ingrese su edad para generar una contraseña: "))
+
+
+
